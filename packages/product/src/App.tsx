57d644aa55323146045317acb687d420d45e2363
kelvi <kelvi_ugo@hotmail.com>
2022-06-08 17:07:14 -0300

    feat: install chakra-ui at products

--- packages/product/src/App.tsx
+++ packages/product/src/App.tsx
@@ -1,14 +1,21 @@
+import { extendTheme, ThemeProvider } from "@chakra-ui/react";
 import React from "react";
 import ReactDOM from "react-dom";
 
 import "./index.css";
+import Marketing from "./pages/Marketing";
+
+const theme = extendTheme({
+  brand: {
+    900: "#1a365d",
+    800: "#153e75",
+    700: "#2a69ac",
+  },
+});
 
 const App = () => (
-  <div className="container">
-    <div>Name: product</div>
-    <div>Framework: react</div>
-    <div>Language: TypeScript</div>
-    <div>CSS: Empty CSS</div>
-  </div>
+  <ThemeProvider theme={theme}>
+    <Marketing />
+  </ThemeProvider>
 );
 ReactDOM.render(<App />, document.getElementById("app"));
