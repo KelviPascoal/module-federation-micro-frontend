b31f8cac6af06306e6d43300604af785b78907b5
kelvi <kelvi_ugo@hotmail.com>
2022-06-08 17:05:54 -0300

    feat: install chakra-ui at customers

--- packages/customers/src/App.tsx
+++ packages/customers/src/App.tsx
@@ -1,12 +1,23 @@
+import { extendTheme, ThemeProvider } from "@chakra-ui/react";
 import React from "react";
 import ReactDOM from "react-dom";
 
 import "./index.css";
 import Customers from "./pages/Customers";
 
+const colors = {
+  brand: {
+    900: "#1a365d",
+    800: "#153e75",
+    700: "#2a69ac",
+  },
+};
+
+const theme = extendTheme({ colors });
+
 const App = () => (
-  <div>
+  <ThemeProvider theme={theme}>
     <Customers></Customers>
-  </div>
+  </ThemeProvider>
 );
 ReactDOM.render(<App />, document.getElementById("app"));
